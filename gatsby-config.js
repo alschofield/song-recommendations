18cea84ee9121c86176acae8aea98519c970c4c9
alschofield93 <alschofield93@gmail.com>
2021-06-18 17:49:03 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    title: "app1",
+  },
+  plugins: [
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
